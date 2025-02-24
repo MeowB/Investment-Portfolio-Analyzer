2f1d4a49201f5bf04968a52db0efa84391874ba9
MeowB <meolearn@gmail.com>
2025-02-24 07:16:33 +0100

    update sqlit path for heroku

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ import os
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = os.path.join(app.root_path, '/instance/stocks_portfolios.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///stocks_portfolios.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 if not os.path.exists('instance/stocks_portfolios.db'):
