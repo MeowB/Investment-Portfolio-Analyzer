86b363825fe325d39f6c5ee7de4eb32e925ab529
Scott Borlon <60074375+MeowB@users.noreply.github.com>
2025-02-24 12:23:03 +0100

    doing some more testing for deployment

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,9 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-	proxy: {
-		'/api': 'https://portfolio-analyzer-9aa13095c684.herokuapp.com'
-	}
-  }
 })
